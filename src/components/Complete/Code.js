b6e868381f6c610cf96bf13959ead03cfd4e9c94
Youngjae Lim <limyoungjae@gmail.com>
2022-03-16 22:25:47 -0400

    Fixed Code component to render a fenced code block without language specification

--- src/components/Complete/Code.js
+++ src/components/Complete/Code.js
@@ -1,21 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Code = ({ children }) => {
-  return (
-    <code
-      style={{
-        background: 'var(--clr-code-bg)',
-        color: 'var(--clr-code-font)',
-        padding: '0.3rem',
-        borderRadius: 'var(--radius)',
-        fontSize: '90%',
-        overflowX: 'auto',
-        whiteSpace: 'nowrap',
-      }}
-    >
-      {children}
-    </code>
-  )
+  return <Pre>{children}</Pre>
 }
 
 export default Code
+
+const Pre = styled.code`
+  background: var(--clr-code-bg);
+  color: var(--clr-code-font);
+  padding: 0.3rem;
+  border-radius: var(--radius);
+  font-size: 90%;
+  overflow-x: auto;
+  white-space: break-spaces;
+  overflow-wrap: break-word;
+`
