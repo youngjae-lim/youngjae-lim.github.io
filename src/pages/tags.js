0cf18695a62a5d8f78ac758ac250a111c0efe788
Youngjae Lim <limyoungjae@gmail.com>
2022-03-30 23:43:18 -0400

    Fixed layout issue on tags page

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -3,7 +3,6 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Seo from '../components/Seo'
 import Posts from '../components/Posts'
-import Banner from '../components/Banner'
 import { myContext } from '../../provider'
 
 const TagsPage = ({ data }) => {
@@ -16,20 +15,7 @@ const TagsPage = ({ data }) => {
   return (
     <Layout>
       <Seo title='All posts' />
-      <section className='posts'>
-        <article>
-          <Posts
-            posts={posts}
-            title='All posts'
-            count={count}
-            context={value}
-          />
-        </article>
-        {/* banner column on the right side */}
-        <article>
-          <Banner post={true} />
-        </article>
-      </section>
+      <Posts posts={posts} title='All posts' count={count} context={value} />
     </Layout>
   )
 }
